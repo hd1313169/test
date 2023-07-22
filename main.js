426c28135985dac312d926ac6b1d655bfd96da0e
Ryan Chiang <wsad71155@gmail.com>
2023-07-22 16:22:16 +0800

    0722

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.5,
+    spaceBetween: 0,
+
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+  },
+});
